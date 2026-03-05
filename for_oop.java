1a2e9ce388c8d5d3307d73abadfdb413dfe97552
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-03-05 23:24:12 +0530

    this is my start

---
+++ for_oop.java
@@ -0,0 +1,14 @@
+import java.util.scanner;
+class apna{
+    public static void main(String[] args){
+    System.out.println("how many integer to read");
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    
+    for(int i=1;i<=n;1++){
+        System.out.println(x:"enter the value of x");
+        int num=sc.nextInt();
+        System.out.println("user enteered"+num);
+    }
+}
+}
