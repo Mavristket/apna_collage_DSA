b6263fc28b69ae47e6be62b76e1b7ea7dd46b6ba
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-03-16 14:18:58 +0530

    Floyd'spattern

---
+++ floyds.java
@@ -0,0 +1,15 @@
+class floyds{
+    public static void pattern(int n){
+        int count=1;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        pattern(5);
+    }
+}
