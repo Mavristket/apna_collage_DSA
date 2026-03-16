2165982fe399c81ed442788c0091fed4aa4b25c2
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-03-16 13:11:27 +0530

    inverted_half_parimed

---
+++ inverted_half_parimed.java
@@ -0,0 +1,18 @@
+class inverted_half_parimed
+{
+ public static void patern(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        for(int k=1;k<=i;k++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    }
+    public static void main(String[] args){
+        patern(5);
+    }
+ }
+
