1600cd71c44d674f174199765e74a3a8fcc9e920
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-03-17 12:32:09 +0530

    Zero One Triangle

---
+++ zero_one_triangle.java
@@ -0,0 +1,18 @@
+class zero_one_triangle{
+    public static void triangle(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("1");
+
+                }else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        triangle(5);
+    }
+}
