731c65d6b19b1ed23709eca62a9628ab1b107d58
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-04-08 20:17:35 +0530

    Bubble sort

---
+++ bubble_sort.java
@@ -0,0 +1,23 @@
+class bubble_sort{
+    public static void sort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void p(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args){
+        int arr[]={9,8,1,2,3,8,6,7};
+        sort(arr);
+        p(arr);
+    }
+}
