375a08fb6600eea4ebf432cad0b6f9127477f64f
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-04-10 17:25:48 +0530

    Built in sort

---
+++ builtinsort.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+class builtinsort{
+    public static void main(String[] args){
+        int arr[]={9,6,1,5,8,7,3};
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+}
