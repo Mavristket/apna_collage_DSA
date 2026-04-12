d3a9a34e6b8e573f32ee74640c4356d7f3a9013c
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-04-12 12:22:24 +0530

    Spiral matrix print

---
+++ spiral_matrix.java
@@ -0,0 +1,44 @@
+class spiral_matrix {
+    public static void spiral(int matrix[][]) {
+        int startrow = 0;
+        int startcol = 0;
+        int endrow = matrix.length - 1;
+        int endcol = matrix[0].length - 1;
+
+        while (startcol <= endcol && startrow <= endrow) {
+
+            for (int j = startcol; j <= endcol; j++) {
+                System.out.print(matrix[startrow][j]);
+            }
+
+            for (int i = startrow + 1; i <= endrow; i++) {
+                System.out.print(matrix[i][endcol]);
+            }
+
+            // fix: avoid duplicate
+            if (startrow < endrow) {
+                for (int j = endcol - 1; j >= startcol; j--) {
+                    System.out.print(matrix[endrow][j]);
+                }
+            }
+
+            // fix: avoid duplicate
+            if (startcol < endcol) {
+                for (int i = endrow - 1; i > startrow; i--) {
+                    System.out.print(matrix[i][startcol]);
+                }
+            }
+
+            // fix: move boundaries
+            startrow++;
+            startcol++;
+            endrow--;
+            endcol--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        spiral(matrix);
+    }
+}
