79a02d7e533ae74a4ea3b16f5b6dfb2a8db8ed99
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-04-15 13:09:35 +0530

    largest string as per alphabets

---
+++ largest_string.java
@@ -0,0 +1,13 @@
+class largest_string{
+    public static void main(String[] args){
+        String arr[]={"balaji","gaikwad","tony","stark"};
+        String largest=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(largest.compareTo(arr[i])<0){
+                largest=arr[i];
+            }
+        }
+        System.out.println("Largest string is: " + largest);
+
+    }
+}
