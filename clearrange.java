0a591964fe68e8c18f50d259aa0281fa2b4072ed
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-04-17 12:22:01 +0530

    Clear range of bit

---
+++ clearrange.java
@@ -0,0 +1,14 @@
+class clearrange{
+    public static int clear(int n,int i,int j){
+        int a=~(0)<<j+1;
+        int b=(1<<i)-1;
+        int bitmask=a|b;
+        return n&bitmask;
+    }
+    public static void main(String[] args){
+        int n=15;
+        int i=1;
+        int j=2;
+        System.out.println(clear(n,i,j));
+    }
+}
