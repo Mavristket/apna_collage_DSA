834865ab9cd8cc42287289d8d34036f1f6fc9188
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-04-18 12:10:26 +0530

    fast fastexponential

---
+++ fastexponential.java
@@ -0,0 +1,18 @@
+class fastexponential{
+    public static void cal(int a,int n){
+        int ans=1;
+        while(n>0){
+            if((n & 1)!=0){
+                ans=ans*a;
+            }
+            a=a*a;
+            n=n>>1;
+        }
+        System.out.println(ans);
+    }
+    public static void main(String[] args){
+        int a=5;
+        int n=3;
+        cal(a,n);
+    }
+}
