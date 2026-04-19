b2967fbb180cde86edbe152ace97906dfeb02301
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-04-19 13:09:39 +0530

    java packages

---
+++ package.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class package{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Scanner sc=new Scanner(System.in);
+    }
+}
