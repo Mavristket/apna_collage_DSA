42c3194a4806e3e3a2e4442b78a206b8fb9b28f3
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-04-22 18:05:00 +0530

    factorial

---
+++ factorial2.java
@@ -0,0 +1,16 @@
+class factorial2 {
+    public static int fact(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int fnm1 = fact(n - 1);
+        int fn = n * fnm1;
+        return fn;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int fn = fact(n);
+        System.out.println(fn);
+    }
+}
