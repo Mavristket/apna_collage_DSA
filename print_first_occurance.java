30ad845cb0f2a9afa1160916da1fafe875d10388
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-04-23 17:29:20 +0530

    print_first_occurance

---
+++ print_first_occurance.java
@@ -0,0 +1,17 @@
+class print_first_occurance{
+    public static int check(int arr[],int key,int i){
+        if(i==arr.length-1){
+            return -1;
+        }
+        if(arr[i]==key){
+            return i;
+        }
+        return check(arr, key, i+1);
+    }
+    public static void main(String[] args){
+        int arr[]={1,2,3,4,5};
+        int key=3;
+        int ans=check(arr, key, 0);
+        System.out.println(ans);
+    }
+}
