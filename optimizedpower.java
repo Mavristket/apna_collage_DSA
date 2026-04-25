19dd465f839d9bc0f8265497f0faa4af18867ff0
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-04-25 11:44:19 +0530

    optimized x to the power n

---
+++ optimizedpower.java
@@ -0,0 +1,19 @@
+class optimizedpower{
+    public static int check(int x,int n){
+        if(n==0){
+            return 1;
+        }
+        int halfpow=check(x,n/2);
+        int halfpowsq=halfpow*halfpow;
+        if(n%2==0){
+            halfpowsq=x*halfpowsq;
+        }
+        return halfpowsq;
+    }
+    public static void main(String[] args){
+        int x=2;
+        int n=5;
+        int result=check(x,n);
+        System.out.print(result);
+    }
+}
