4ad1023e452eaa6eb548f3a4d0abb0b513bba1f6
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-04-28 17:49:07 +0530

    marge sort modofied

--- Divide_and_concquer/marge_sort.java
+++ Divide_and_concquer/marge_sort.java
@@ -34,6 +34,7 @@ class marge_sort{
     public static void main(String[] args) {
         int arr[]={5,4,3,2,1};
         marge_s(arr, 0, arr.length-1);
+        System.out.println("Sorted array is:");
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
