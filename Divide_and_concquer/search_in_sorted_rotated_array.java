0c697c1c2ad2ba35420db70f90dd26563ec9d85e
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-04-30 17:33:25 +0530

    search_in_sorted_rotated_array.java

---
+++ Divide_and_concquer/search_in_sorted_rotated_array.java
@@ -0,0 +1,38 @@
+class search_in_sorted_rotated_array{
+    public static int search(int arr[],int target,int start,int end){
+        if(start>end){
+            return -1;
+        }
+
+        int mid=(start+end)/2;
+
+        if(arr[mid]==target){
+            return mid;
+        }
+
+        if(arr[start]<arr[mid]){
+            if(arr[start]<=target && target<arr[mid]){
+                return search(arr,target,start,mid-1);
+            }else{
+                return search(arr,target,mid+1,end);
+            }
+        }else{
+            if(arr[mid]<=target && target<=arr[end]){
+                return search(arr,target,mid+1,end);
+            }else{
+                return search(arr,target,start,mid-1);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={4,5,6,7,0,1,2};
+        int target=0;
+        int n=arr.length;
+        int result=search(arr,target,0,n-1);
+        if(result==-1){
+            System.out.println("Element not found in the array.");
+        }else{
+            System.out.println("Element found at index: "+result);
+        }
+    }
+}
