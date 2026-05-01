9f6cd36f4ccb17ae94402d741775c4f80ba92b87
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-05-01 13:15:34 +0530

    Marge sort on the string array

--- Divide_and_concquer/marrge_string_array.java
+++ Divide_and_concquer/marrge_string_array.java
@@ -55,5 +55,6 @@ class marrge_string_array {
         for (int i = 0; i < result.length; i++) {
             System.out.print(result[i] + " ");
         }
+        System.out.print("Time Complexity: O(n log n)");
     }
 }
