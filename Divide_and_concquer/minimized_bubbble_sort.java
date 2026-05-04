4f628b716962feae5d8d5aa17850883ed1b5c439
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-05-04 17:49:48 +0530

    minimized_bubbble_sort

---
+++ Divide_and_concquer/minimized_bubbble_sort.java
@@ -0,0 +1,27 @@
+class minimized_bubbble_sort{
+    public static void bubble_sort(int arr[]){
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            boolean swapped=false;
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    swapped=true;
+                }
+            }
+            if(swapped==false){
+                break;
+            }
+        }
+    }
+    public static void main(String[] args){
+        int arr[]={5,4,3,2,1};
+        bubble_sort(arr);
+        System.out.println("Sorted array is:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
