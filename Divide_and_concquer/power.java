4890315ce0f6977647a15d95b0c8b5553c251815
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-05-07 16:40:25 +0530

    power finding

---
+++ Divide_and_concquer/power.java
@@ -0,0 +1,18 @@
+class power{
+    public static int power(int a,int n){
+        if(n==0){
+            return 1;
+        }
+        int half=power(a,n/2);
+        int full=half*half;
+        if(n%2!=0){
+            full=full*a;
+        }   
+        return full;
+    }
+    public static void main(String[] args) {
+        int a=2;
+        int n=10;
+        System.out.println(a+" raised to the power "+n+" is: "+power(a,n));
+    }
+}
