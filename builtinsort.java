5a2cb5c7bdce85176c5e3d18b1bd6569baf0134e
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-05-08 18:53:04 +0530

    Complexity of the built in sort

--- builtinsort.java
+++ builtinsort.java
@@ -6,6 +6,7 @@ class builtinsort{
         for(int i=0;i<arr.length;i++){
             System.out.println(arr[i]);
         }
+        System.out.println("Complexity of this algorithm is O(n log n) because it uses a dual-pivot quicksort algorithm which has an average and worst-case time complexity of O(n log n).");   
     }
 
 }
