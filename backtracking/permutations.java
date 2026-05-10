092ce272fdc4a2e08f19796fb9c77b317df0c8b6
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-05-10 12:43:52 +0530

    Permutations

---
+++ backtracking/permutations.java
@@ -0,0 +1,19 @@
+class permutations{
+    public static void check(String str,String ans){
+
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        for(int i=0;i<str.length();i++){
+            String curr=str.charAt(i)+"";
+            String newStr=str.substring(0,i)+str.substring(i+1);
+            check(newStr,ans+curr);
+        }
+    }
+    public static void main(String[] args) {
+        String str="abc";
+        check(str,"");
+    }
+    
+}
