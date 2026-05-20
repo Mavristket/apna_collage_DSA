68325277717b88f08b159b0e53c23b51cf046505
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-05-20 11:13:17 +0530

    Pair sum optimized

---
+++ backtracking/pair_sum_optimized.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class pair_sum_optimized{
+    public static boolean test(ArrayList<Integer> list,int target){
+        int lp=0;
+        int rp=list.size()-1;
+        while(lp<rp){
+            if(list.get(lp)+list.get(rp)==target){
+                return true;
+            }
+            if(list.get(lp)+list.get(rp)<target){
+                lp++;
+            }else{
+                rp--;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(60);
+        int target=40;
+        System.out.print(test(list,target));
+    }
+}
