ce50fddd412cd2ee6135cd98aac3996d141f7f39
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-05-20 11:05:43 +0530

    Pair sum brut force

---
+++ backtracking/pair_sum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class pair_sum{
+    public static boolean test(ArrayList<Integer> list,int target){
+        for(int i=0;i<list.size();i++){
+            for(int j=i+1;j<list.size();j++){
+                if(list.get(i)+list.get(j)==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(60);
+        int target=40;
+        System.out.print(test(list,target));
+
+    }
+
+}
