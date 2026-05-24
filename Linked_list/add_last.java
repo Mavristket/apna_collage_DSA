113f234d7c9c9f2095d8c7dab9fa71a9cfb0a310
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-05-24 13:02:44 +0530

    linked list adding element at last node

---
+++ Linked_list/add_last.java
@@ -0,0 +1,37 @@
+public class add_last{
+    public class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    Node head=null;
+    Node tail=null;
+
+    void add(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        tail.next=newNode;
+        tail=newNode;
+    }
+    void display(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        add_last a=new add_last();
+        a.add(10);
+        a.add(20);
+        a.add(30);
+        a.display();
+    }
+}
