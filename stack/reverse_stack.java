021e4038f1b7f701c88a2631e0c6a3a8968d251f
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-06-11 18:56:02 +0530

    Reverse the stack

---
+++ stack/reverse_stack.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class reverse_stack{
+    public static void reverse(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top=s.pop();
+        reverse(s);
+        insertAtBottom(s,top);
+    }
+    public static void insertAtBottom(Stack<Integer> s,int top){
+        if(s.isEmpty()){
+            s.push(top);
+            return;
+        }
+        int curr=s.pop();
+        insertAtBottom(s,top);
+        s.push(curr);
+    }
+    public static void main(String[] args){
+        Stack<Integer> s=new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        reverse(s);
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+    }
+}
