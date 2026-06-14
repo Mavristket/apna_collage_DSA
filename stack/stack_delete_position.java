96c363bb636b0cc4526a75c80054576156f05d6d
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-06-14 19:49:16 +0530

    delete the element at specific position

---
+++ stack/stack_delete_position.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+
+        int position = 3; // Delete element at position 3 (from top)
+
+        Stack<Integer> temp = new Stack<>();
+
+        // Move elements above the position to temp stack
+        for (int i = 1; i < position; i++) {
+            temp.push(stack.pop());
+        }
+
+        // Delete the element
+        System.out.println("Deleted Element: " + stack.pop());
+
+        // Put elements back
+        while (!temp.isEmpty()) {
+            stack.push(temp.pop());
+        }
+
+        System.out.println("Stack after deletion: " + stack);
+    }
+}
