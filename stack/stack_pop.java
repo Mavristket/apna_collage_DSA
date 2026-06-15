26c6447d7e0b745ab5bd0161cca61da71c9487f3
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-06-15 17:02:50 +0530

    Stack pooping

---
+++ stack/stack_pop.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class ReverseStack {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> reverseStack = new Stack<>();
+
+        // Push elements into original stack
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+
+        System.out.println("Original Stack: " + stack);
+
+        // Reverse the stack
+        while (!stack.isEmpty()) {
+            reverseStack.push(stack.pop());
+        }
+
+        System.out.println("Reversed Stack: " + reverseStack);
+    }
+}
