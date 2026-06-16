fce93110fd0fc417ff18c560d1a7e9b2858b43d1
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-06-16 22:01:02 +0530

    reverse stack

--- stack/reverse_stack.java
+++ stack/reverse_stack.java
@@ -19,6 +19,7 @@ public class reverse_stack{
     }
     public static void main(String[] args){
         Stack<Integer> s=new Stack<>();
+        System.out.println("Original Stack: " + s);
         s.push(1);
         s.push(2);
         s.push(3);
