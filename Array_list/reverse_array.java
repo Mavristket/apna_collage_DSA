610b06eced4c18d4b4ce3f83bdc415b180410d69
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-06-22 22:01:11 +0530

    reverse array

--- Array_list/reverse_array.java
+++ Array_list/reverse_array.java
@@ -8,6 +8,8 @@ class reverse_array{
         list.add(40);
         for(int i=list.size()-1;i>=0;i--){
             System.out.print(list.get(i));
+
         }
+        System.out.println("O(n)");
     }
 }
