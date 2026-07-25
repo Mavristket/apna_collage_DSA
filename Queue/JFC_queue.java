6715ef249ab10a6e9c5df0f71bc6c093de6a3479
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-07-25 11:56:08 +0530

    Java in  buit queue

---
+++ Queue/JFC_queue.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class JFC_queue{
+    public static void main(String[] args){
+        Queue<Integer> q=new LinkedList<>();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+
+        while(!q.isEmpty()){
+            System.out.print(q.peek());
+            q.remove();
+        }
+    }
+}
