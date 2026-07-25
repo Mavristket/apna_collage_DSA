a31bc51efda325b18ffc3c8f603df7a28014c227
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-07-25 12:16:35 +0530

    queue using the stack

---
+++ Queue/queue_using_stack.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class queue_using_stack{
+    public static Stack<Integer> s1=new Stack<>();
+    public static Stack<Integer> s2=new Stack<>();
+    public static boolean isEmpty(){
+        return s1.isEmpty();
+    }
+
+    public void add(int data){
+        while(!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
+        s1.push(data);
+        while(!s2.isEmpty()){
+            s1.push(s2.pop());
+        }
+        
+    }
+    public static int remove(){
+        if(isEmpty()){
+            System.out.print("Queue is empty");
+            return -1;
+        }
+        return s1.pop();
+    }
+    public static int peek(){
+        if(isEmpty()){
+            System.out.print("Queue is empty");
+            return -1;
+        }
+        return s1.peek();
+    }
+    public static void main(String[] args){
+        queue_using_stack q=new queue_using_stack();
+        q.add(1);
+        q.add(2);
+        while(!q.isEmpty()){
+            System.out.print(q.peek());
+            q.remove();
+        }
+    }
+}
