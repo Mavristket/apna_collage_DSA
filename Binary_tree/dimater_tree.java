e0de1bd1e642d45b74537fde60f6230d399ea46a
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-08-02 12:32:49 +0530

    Dimeter of the tree

--- Binary_tree/dimater_tree.java
+++ Binary_tree/dimater_tree.java
@@ -31,12 +31,34 @@ public class dimater_tree{
         int selfdim=lh+rh+1;
         return Math.max(selfdim,Math.max(ld,rd));
     }
+    static class Info{
+        int diam;
+        int ht;
+        Info(int diam,int ht){
+            this.diam=diam;
+            this.ht=ht;
+        }
+    }
+    public static Info dimater2(Node root){
+        if(root==null){
+            return new Info(0,0);
+
+        }
+        Info leftInfo=dimater2(root.left);
+        Info rightInfo=dimater2(root.right);
+
+        int selfDiam=leftInfo.ht+rightInfo.ht+1;
+        int diam=Math.max(selfDiam,Math.max(leftInfo.diam,rightInfo.diam));
+        int ht=Math.max(leftInfo.ht,rightInfo.ht)+1;
+
+        return new Info(diam,ht);
+    }
     public static void main(String[] args){
         Node root=new Node(1);
         root.left=new Node(2);
         root.right=new Node(3);
         root.left.left=new Node(4);
         root.left.right=new Node(5);
-        System.out.println("Dimater of the binary tree: " + dimeter(root));
+        System.out.println("Dimater of the binary tree: " + dimater2(root).diam);
     }
 }
