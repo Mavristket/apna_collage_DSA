a6c64bfb1f2e47954af0d5203768f70304e31399
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-08-03 21:53:06 +0530

    K th  level elements in the Binary tree

---
+++ Binary_tree/kth_element.java
@@ -0,0 +1,31 @@
+public class kth_element{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public static void level(Node root,int level,int k){
+        if(level==k){
+            System.out.print(root.data+" ");
+            return;
+        }
+        level(root.left,level+1,k);
+        level(root.right,level+1,k);
+
+    }
+    public static void main(String[] args){
+        Node root=new Node(1);
+        root.left=new Node(2);
+        root.right=new Node(3);
+        root.left.left=new Node(4);
+        root.left.right=new Node(5);
+        int k=2;
+        level(root,0,k);
+    }
+}
