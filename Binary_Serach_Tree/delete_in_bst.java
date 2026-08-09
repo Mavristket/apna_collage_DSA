20ac0db14fc5b0ebe932164fb01fc6f5b02225fd
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-08-09 11:24:58 +0530

    delete node in the BSt

---
+++ Binary_Serach_Tree/delete_in_bst.java
@@ -0,0 +1,61 @@
+import java.util.*;
+public class delete_in_bst{
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public static Node insert(Node root,int val){
+        if(root==null){
+            Node newNode=new Node(val);
+            return newNode;
+        }
+        if(val<root.data){
+            root.left=insert(root.left,val);
+        }else{
+            root.right=insert(root.right,val);
+        }
+        return root;
+    }
+    public static Node find(Node root){
+        while(root.left!=null){
+            root=root.left;
+        }
+        return root;
+    }
+    public static Node delete(Node root,int val){
+        if(root.data<val){
+            root.right=delete(root.right,val);
+        }else if(root.data>val){
+            root.left=delete(root.left,val);
+        }else{
+            if(root.left==null && root.right==null){
+                return null;
+            }
+            if(root.left==null){
+                return root.right;
+            }else if(root.right==null){
+                return root.left;
+            }
+            Node IS=find(root.right);
+            root.data=IS.data;
+            root.right=delete(root.right,IS.data);
+        }
+        return root;
+    }
+    public static void main(String[] args){
+        int arr[]={5,1,3,4,2,7};
+        Node root=null;
+        for(int i=0;i<arr.length;i++){
+            root=insert(root,arr[i]);
+        }
+        System.out.print(delete(root,7));
+        
+    }
+}
