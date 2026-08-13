46018e56a5c90bfac1bde8d9a516b4ebfd256461
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-08-13 18:11:00 +0530

    priority queue

---
+++ Heap/priority_queue.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class priority_queue{
+    public static void main(String[] args){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(10);
+        pq.add(20);
+        pq.add(15);
+        System.out.println(pq.peek()); // Prints the smallest element
+        System.out.println(pq.poll()); // Removes and prints the smallest element
+        System.out.println(pq.peek()); // Prints the next smallest element
+    }
+
+}
