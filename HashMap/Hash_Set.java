7950bc4e82dc38df42a89b92da262c2b51569e32
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-08-20 20:13:47 +0530

    Hash set in java

---
+++ HashMap/Hash_Set.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hash_Set{
+    public static void main(String[] args){
+        HashSet<Integer> h=new HashSet<>();
+
+        h.add(10);
+        h.add(20);
+        h.add(20);
+        h.remove(10);
+        System.out.print(h);
+        
+            }
+}
