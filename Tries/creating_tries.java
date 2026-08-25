c68eabf9c43d9c34850bb49ef1bada6ba11a59f7
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-08-25 19:53:04 +0530

    insert in the tries

--- Tries/creating_tries.java
+++ Tries/creating_tries.java
@@ -11,4 +11,22 @@ public class creating_tries{
         }
     }
     public static Node root=new Node();
+    public static void insert(String word){
+        Node curr=root;
+        for(int level=0;level < word.length();level++){
+            int idx=word.charAt(level)-'a';
+            if(curr.childeren[idx]==null){
+                curr.childeren[idx]=new Node();
+            }
+            curr=curr.childeren[idx];
+        }
+          curr.eow = true;
+    }
+    public static void main(String[] args){
+        String words[]={ "the","therir","theree"};
+
+        for(int i=0;i<words.length;i++){
+            insert(words[i]);
+        }
+    }
 }
